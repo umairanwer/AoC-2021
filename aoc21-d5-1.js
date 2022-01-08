06f3251d8fc42d9ca34229794e1a6e5c8205bb25
umairanwer <92umair@gmail.com>
2022-01-09 02:00:18 +0500

    working

--- aoc21-d5-1.js
+++ aoc21-d5-1.js
@@ -8,39 +8,45 @@ let inputFile = fs.readFileSync('aoc21-d5inp.txt', 'utf8', (err, data) => {
 
 let fileInput = inputFile.split('\n');
 fileInput.pop();    //remove empty end line
-console.log(fileInput[fileInput.length-1]);
 let gridSz = 1000;
 
 let grid = Array(gridSz).fill().map(() => Array(gridSz).fill(0)); //outer dimension for y-axis, inner for x-axis
+console.log(`Total Entries: ${fileInput.length}`)
 
 for(let [index, line] of fileInput.entries()){    //entries function returns an iterator for an array
     //if(index == 1) break;
     let linePts = line.split(" -> ");
-    let [x1,y1,x2,y2] = [...linePts[0].split(','), ...linePts[1].split(',')];
+    let [x1, y1, x2, y2] = [...linePts[0].split(','), ...linePts[1].split(',')].map(Number);
+    
+    //console.log(line, [x1, y1, x2, y2])
 
     //x1 and y1 to always be less than x2 and y2 for ease of calcs
     if(x2 < x1) [x1, x2] = [x2, x1];    //ONLY for hor lines
     if(y2 < y1) [y1, y2] = [y2, y1];    //ONLY for hor lines
 
-    if(x1 != x2 && y1 != y2)    continue;   //neglecting non horizontal lines
-
+    if(x1 !== x2 && y1 !== y2){  //neglecting non horizontal lines
+        //console.log('Non-hor: ' + [x1,y1,x2,y2]);
+        continue; 
+    }
     let yL = 0;
+    if((x1-x2) > 0 || (y1-y2) >0)   console.log('prob:' +[x1,y1,x2,y2]);
+
     for(let yAx = y1; yAx <= y2; yAx++){
         let xL = 0;
         yL++;
-        //console.log(`yAx:${yAx}`);
+        
         for(let xAx = x1; xAx <= x2; xAx++){
             xL++;
             grid[yAx][xAx]++; 
-            //console.log(`yAx:${yAx}, xAx=${xAx}`);
         }
-        //console.log(line);
-        //printGrid(grid);
-        //console.log([x1,y1,x2,y2, xL, yAx]);
     }
 
 }
-console.log(grid.map((axis) => axis.filter((val)=>val>=2)).map((axis)=> axis.length).reduce((acc, val)=>acc+val, 0));
+
+//output no of values >= 2
+console.log(grid.map((axis) => axis.filter((val)=>val>=2))
+                .map((axis)=> axis.length)
+                .reduce((acc, val)=>acc+val, 0));
 //printGrid(grid);
 
 function printGrid(grid){
